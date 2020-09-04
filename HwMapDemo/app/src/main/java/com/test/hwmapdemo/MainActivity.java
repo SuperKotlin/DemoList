b38ad513d32de3da44e6858c68fddf6c9aad49e2
99305919@qq.com <ZXCVBNM.12>
2020-09-04 20:48:20 +0800

    commit

--- HwMapDemo/app/src/main/java/com/test/hwmapdemo/MainActivity.java
+++ HwMapDemo/app/src/main/java/com/test/hwmapdemo/MainActivity.java
@@ -5,15 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.huawei.hms.maps.CameraUpdateFactory;
 import com.huawei.hms.maps.HuaweiMap;
 import com.huawei.hms.maps.MapView;
 import com.huawei.hms.maps.OnMapReadyCallback;
+import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.MarkerOptions;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = "MainActivity";
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
     private MapView mMapView;
     private HuaweiMap hMap;
+
+    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(HuaweiMap map) {
         Log.d(TAG, "onMapReady: ");
         hMap = map;
+
+        hMap.setBuildingsEnabled(true);
+        hMap.setOnMapClickListener(new HuaweiMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                hMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
+                hMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        });
+        addMarker();
     }
 
+    private void addMarker() {
+        int maxLat = 90;
+        int maxLng = 180;
+
+        for (int i = 0; i < 1000; i++) {
+
+            int lat = random.nextInt(maxLat) % (maxLat + maxLat + 1) - maxLat;
+            int lng = random.nextInt(maxLng) % (maxLng + maxLng + 1) - maxLng;
+
+            LatLng latLng1 = new LatLng(lat, lng);
+            hMap.addMarker(new MarkerOptions().position(latLng1).clusterable(true));
+        }
+
+        hMap.setMarkersClustering(true);
+    }
 
     @Override
     protected void onStart() {
